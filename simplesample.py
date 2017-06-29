547710431d67d8f2c1386c646b78bdad39a7ca76
Stephan Looney <ablinkin@me.com>
2017-06-29 08:30:03 -0400

    Initial files

---
+++ simplesample.py
@@ -0,0 +1,11 @@
+import json
+import os
+import sys
+
+filetoimport = "example.json"
+with open('example.json','r') as json_data:
+    users = json.load(json_data)
+    for user in users:
+        print(user['firstname'] + " " + user['lastname'])
+        for pet in user['pets']:
+            print pet
